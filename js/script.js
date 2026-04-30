5adab8994e3c09606af72427796b35d84ba05193
Hunter Funk <hlfunk2008@gmail.com>
2026-04-30 11:15:59 -0400

    started js development

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Test")
+let nameField = document.querySelector('label[for="name"]')
+let jobField = document.querySelector('select[id="title"]')
+let otherJobField = document.querySelector('input[name="other-job-role"]')
+otherJobField.setAttribute("hidden", null)
+window.onload = function () {
+    nameField.focus()
+}
+
+jobField.addEventListener("change", (event) => {
+    if (event.target.value === "other") {
+        document.querySelector('option[hidden]').removeAttribute("hidden");
+        otherJobField.removeAttribute("hidden");
+    } 
+})
+
+// Finished step 1, partial step 2
