98a66710e2d7e4b86fc91e293dc43a0be5341072
Hunter Funk <hlfunk2008@gmail.com>
2026-05-04 11:11:30 -0400

    added changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
-let nameField = document.querySelector('label[for="name"]')
-let jobField = document.querySelector('select[id="title"]')
-let otherJobField = document.querySelector('input[name="other-job-role"]')
-otherJobField.setAttribute("hidden", null)
+// let nameField = document.querySelector('label[for="name"]')
+// let jobField = document.querySelector('select[id="title"]')
+// let otherJobField = document.querySelector('input[name="other-job-role"]')
+// otherJobField.setAttribute("hidden", null)
+// window.onload = function () {
+//     nameField.focus()
+// }
+
+// jobField.addEventListener("change", (event) => {
+//     if (event.target.value === "other") {
+//         document.querySelector('option[hidden]').removeAttribute("hidden");
+//         otherJobField.removeAttribute("hidden");
+//     } 
+// })
+
+let labelName = document.querySelector('label[for="name"]');
+let jobSelect = document.querySelector('#title');
+let otherJob = document.querySelector('input[name="other-job-role"]');
+
+let colorDiv = document.querySelector('div[id="shirt-colors"]');
+let colorSelect = document.querySelector('#color');
+let designLabel = document.querySelector('select[id="design"]')
+
+otherJob.setAttribute("hidden", "");
+
 window.onload = function () {
-    nameField.focus()
+    labelName.focus();
 }
 
-jobField.addEventListener("change", (event) => {
+jobSelect.addEventListener("change", (event) => {
     if (event.target.value === "other") {
         document.querySelector('option[hidden]').removeAttribute("hidden");
-        otherJobField.removeAttribute("hidden");
-    } 
+        otherJob.removeAttribute("hidden")
+    } else {
+        otherJob.setAttribute("hidden", "")
+    }
 })
 
-// Finished step 1, partial step 2
+colorDiv.setAttribute("hidden", "")
+
+designLabel.addEventListener("change", (event) => {
+    colorDiv.removeAttribute("hidden")
+
+    if (event.target.value === "js puns") {
+        
+    }
+})
